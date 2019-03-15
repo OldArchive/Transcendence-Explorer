6894ae1bf670565223c4e424d99583d3d7143fd6
Akshay <akshaycm@hotmail.com>
2019-03-15 15:04:49 +0300

    telos coin links

--- client/component/Card/CardLinks.jsx
+++ client/component/Card/CardLinks.jsx
@@ -5,14 +5,13 @@ import Card from './Card';
 
 const CardLinks = () => (
   <Card title="Links">
-    <a href="https://dogec.io/" target="_blank">Website</a><br />
-    {/* <a href="https://bitcointalk.org/" target="_blank">Bitcointalk</a><br /> */}
-    <a href="https://github.com/dogecash/dogecash" target="_blank">Github</a><br />
+    <a href="http://teloscoin.org/" target="_blank">Website</a><br />
+    <a href="https://bitcointalk.org/index.php?topic=4453455.0" target="_blank">Bitcointalk</a><br />
+    <a href="https://github.com/phoenixkonsole/transcendence" target="_blank">Github</a><br />
     <a href="https://www.reddit.com/r/DogeCash/" target="_blank">Reddit</a><br />
-    <a href="https://discord.dogec.io/" target="_blank">Discord</a><br />
-    <a href="https://telegram.dogec.io/" target="_blank">Telegram</a><br />
-    <a href="https://twitter.dogec.io/" target="_blank">Twitter</a><br />
-    <a href="https://steemit.dogec.io/" target="_blank">Steemit</a>
+    <a href="https://discord.gg/aeKFNdW" target="_blank">Discord</a><br />
+    <a href="https://twitter.com/teloscoin" target="_blank">Twitter</a><br />
+    {/* <a href="https://steemit.dogec.io/" target="_blank">Steemit</a> */}
   </Card>
 );
 
