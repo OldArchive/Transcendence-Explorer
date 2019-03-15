1e3bf2def767cfe0d6ec94e9be397cd2fa6737b7
Akshay <akshaycm@hotmail.com>
2019-03-15 15:04:34 +0300

    add exchanges

--- client/component/Card/CardExchanges.jsx
+++ client/component/Card/CardExchanges.jsx
@@ -6,9 +6,13 @@ import Card from './Card';
 const CardExchanges = () => (
   <Card title="Exchanges">
 
-    <a href="https://cb.dogec.io/" target="_blank">CryptoBridge</a><br />
-    <a href="https://stex.dogec.io/" target="_blank">STEX</a><br />
-
+    <a href="https://graviex.net/markets/telosbtc" target="_blank">Graviex</a><br />
+    <a href="https://crex24.com/exchange/TELOS-BTC" target="_blank">Crex24</a><br />
+    <a href="https://wallet.escodex.com/market/ESCODEX.TELOS_ESCODEX.BTC" target="_blank">Escodex</a><br />
+    <a href="https://www.southxchange.com/Market/Book/TELOS/BTC" target="_blank">SouthXchange</a><br />
+    <a href="https://bitebtc.com/trade/telos_btc" target="_blank">BiteBTC</a><br />
+    <a href="https://www.finexbox.com/market/pair/TELOS-BTC.html" target="_blank">Finexbox</a><br />
+    <a href="https://coinroom.com/" target="_blank">Coinroom</a><br />
   </Card>
 );
 
