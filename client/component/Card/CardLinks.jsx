4f9233ea4ee61731c0ecae327e63aaea50b0732d
Akshay <akshaycm@hotmail.com>
2019-03-21 12:45:24 +0300

    update reddit

--- client/component/Card/CardLinks.jsx
+++ client/component/Card/CardLinks.jsx
@@ -8,7 +8,7 @@ const CardLinks = () => (
     <a href="http://teloscoin.org/" target="_blank">Website</a><br />
     <a href="https://bitcointalk.org/index.php?topic=4453455.0" target="_blank">Bitcointalk</a><br />
     <a href="https://github.com/phoenixkonsole/transcendence" target="_blank">Github</a><br />
-    <a href="https://www.reddit.com/r/Telos/" target="_blank">Reddit</a><br />
+    <a href="https://www.reddit.com/r/TelosCoin/" target="_blank">Reddit</a><br />
     <a href="https://discord.gg/aeKFNdW" target="_blank">Discord</a><br />
     <a href="https://twitter.com/teloscoin" target="_blank">Twitter</a><br />
     {/* <a href="https://steemit.dogec.io/" target="_blank">Steemit</a> */}
